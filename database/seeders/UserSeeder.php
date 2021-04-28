163cddb5c67ebb0d89053fb798364e18e0722075
Aditya Putra <adtyaputra1999@gmail.com>
2021-04-28 20:47:44 +0800

    Update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,14 @@ class UserSeeder extends Seeder
             'name' => 'Aditya Putra',
             'username' => 'adityaptrp',
             'password' => bcrypt('12345'),
-            'email' => 'adtyaputra1999@gmail.com'
+            'email' => 'adtyaputra1999@gmail.com',
+            'bio' => 'Student at ITB STIKOM BALI, art enthusiast, newbie programmer',
+            'instagram' => 'adityaptrp',
+            'twitter' => 'adityaptrp_',
+            'facebook' => 'adityaptrp.me',
+            'website' => 'https://www.youtube.com/watch?v=zOUtK--JPhc',
+            'youtube_link' => 'UC62n-2EB6U7cPm4yrjBBsPA',
+            'is_admin' => true,
         ]);
     }
 }
